c7f22d0eb642ce886bee34a2f012062be455ed40
ThommyBro <thomas.brockt@gmail.com>
2026-07-03 08:59:33 +0200

    add str method

--- grade_management/course.py
+++ grade_management/course.py
@@ -8,6 +8,10 @@ class Course:
         passing_grade: float = 50.0
 
         def __eq__(self, other):
+            """Compare Courses by ID."""
             if not isinstance(other, Course):
                 return NotImplemented
             return self.course_id == other.course_id
+        
+        def __str__(self):
+             return f"Course('{self.name}', {self.course_id})"
