62f1a0734de01075ac04637daae2702fdb1d996a
ThommyBro <thomas.brockt@gmail.com>
2026-07-03 08:32:36 +0200

    dont even know what these files do

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,12 +1,2 @@
 import grade_management.student as student
 
-class TestStudent:
-
-    # validate function if firstname is empty    
-    def validate_first_name(self, student):
-        assert student.first_name != None
-    
-
-    # validate function if lastname is empty    
-    def validate_last_name(self, student):
-        assert student.last_name != None
