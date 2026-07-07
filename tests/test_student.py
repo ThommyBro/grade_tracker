37800eda65bba68156291e72417267ab6550e03d
ThommyBro <thomas.brockt@gmail.com>
2026-07-07 17:41:30 +0200

    add test for student creation

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,2 +1,21 @@
-import grade_management.student as student
+from grade_management.student import Student
+
+
+def test_valid_student_creation():
+  studi = Student("Jane", "Doe", "jane.doe@example.com")
+  assert studi.email == "jane.doe@example.com"
+  assert studi.full_name == "Jane Doe"
+
+def test_empty_firstname():
+    studi = Student("", "Doe", "jane.doe@example.com")
+    #studi.first_name == ""
+
+def test_empty_lastname():
+    studi = Student("Jane", "", "jane.doe@example.com")
+    #studi.last_name == ""
+
+def test_false_email():
+   studi = Student("Jane", "Doe", "jane.doe.com")
+   
+   
 
