a3249c161075800c15c0b956d2e428e65935736e
ThommyBro <thomas.brockt@gmail.com>
2026-07-16 15:32:38 +0200

    add statistic methods

--- grade_db/statistics_repository.py
+++ grade_db/statistics_repository.py
@@ -24,3 +24,55 @@ class StatisticsRepository:
             }
     
     
+    def count_students_per_course(self):
+        rows = self.conn.execute(
+            """
+            SELECT
+                course_id,
+                COUNT(student_id) AS count
+            FROM grades
+            GROUP BY course_id
+            """
+        )
+
+        return {
+            course_id: count
+            for course_id, count in rows
+        }
+    
+
+    def average_per_student(self):
+        rows = self.conn.execute(
+            """
+            SELECT 
+                student_id,
+                AVG(score)
+            FROM grades
+            GROUP BY student_id
+            """
+            )
+        
+        # return a dict with student_id keys and average scores as values
+        return {
+            student_id: round(avg,2)
+            for student_id, avg in rows
+            }
+    
+
+    def best_students(self):
+        rows = self.conn.execute(
+            """
+            SELECT TOP 5
+                student_id,
+                AVG(score) AS average_score
+            FROM grades
+            GROUP BY student_id
+            ORDER BY average_score DESC;
+            """
+        )
+        # return a dict with student_id keys and average scores as values
+        return {
+            student_id: round(avg,2)
+            for student_id, avg in rows
+            }
+        
