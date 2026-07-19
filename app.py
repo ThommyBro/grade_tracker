20d2a86913c8285b02b181a53427be62197b4a7e
ThommyBro <thomas.brockt@gmail.com>
2026-07-19 15:02:25 +0200

    add app.py for frontend functionality

---
+++ app.py
@@ -0,0 +1,150 @@
+import sqlite3
+import gradio as gr
+
+
+from grade_management.gradebook import GradeBook
+
+from grade_db.student_repository import StudentRepository
+from grade_db.course_repository import CourseRepository
+from grade_db.grade_repository import GradeRepository
+
+from grade_store.sqlite_store import SqliteGradeStore
+
+from sample_data import create_test_data, populate_database
+
+
+def create_sqlite_store():
+
+    # Use database
+    con = sqlite3.connect("grade.db")
+    con.execute("PRAGMA foreign_keys = ON")
+
+    # Create all repos
+    student_repo = StudentRepository(con)
+    course_repo = CourseRepository(con)
+    grade_repo = GradeRepository(con)
+
+    # Create all tables
+    student_repo.create_table()
+    course_repo.create_table()
+    grade_repo.create_table()
+
+    store = SqliteGradeStore(student_repo, course_repo, grade_repo)
+
+    return store, student_repo, course_repo, grade_repo
+
+
+
+def initialize_database(student_repo: StudentRepository, course_repo: CourseRepository, grade_repo: GradeRepository):
+
+    if len(student_repo.get_all()) == 0:
+        print("Empty database, loading sample data...")
+
+        data = create_test_data()
+        populate_database(student_repo, course_repo, grade_repo, data)
+
+    else:
+        print("Database already contains data.")
+
+
+
+def load_gradebook(store):
+
+    gradebook = GradeBook()
+
+    students = {}
+
+    for student in store.get_all_students():
+        gradebook.add_student(student)
+        students[student.student_id] = student
+
+
+    courses = {}
+
+    for course in store.get_all_courses():
+        gradebook.add_course(course)
+        courses[course.course_id] = course
+
+
+    for grade in store.get_all_grades():
+
+        grade.student = students[grade.student.student_id]
+        grade.course = courses[grade.course.course_id]
+
+        gradebook.grades.append(grade)
+
+
+    return gradebook
+
+
+def show_summary(gradebook):
+    return (
+        f"Students: {len(gradebook.students)}\n"
+        f"Courses: {len(gradebook.courses)}\n"
+        f"Grades: {len(gradebook.grades)}"
+    )
+
+
+def get_dashboard_stats(gradebook):
+    return (
+        f"### 👨‍🎓 Students\n{len(gradebook.students)}",
+        f"### 📚 Courses\n{len(gradebook.courses)}",
+        f"### 📝 Grades\n{len(gradebook.grades)}",
+    )
+
+
+
+def main():
+    # Initializing stuff
+    store, student_repo, course_repo, grade_repo = create_sqlite_store()
+    initialize_database(student_repo, course_repo, grade_repo)
+
+    gradebook = load_gradebook(store)
+
+    # Some Theme tests
+    soft = gr.themes.Soft(primary_hue="blue", secondary_hue="slate")
+    glass = gr.themes.Glass()
+    
+
+
+    # Gradio 
+    with gr.Blocks(theme = soft) as demo:
+        gr.Markdown(
+            """
+            # 🎓 Grade Tracker Dashboard
+            Manage students, courses and grades
+            """
+        )
+
+        with gr.Row():
+            with gr.Column():
+                students_card = gr.Markdown(
+                    "Students: -"
+                )
+
+            with gr.Column():
+                courses_card = gr.Markdown(
+                    "Courses: -"
+                )
+
+            with gr.Column():
+                grades_card = gr.Markdown(
+                    "Grades: -"
+                )
+
+        refresh = gr.Button("🔄 Load Dashboard")
+
+        refresh.click(
+                        fn=lambda: get_dashboard_stats(gradebook),
+                        outputs=[students_card, courses_card, grades_card]
+                    )
+        
+
+
+    # start Gradio App
+    demo.launch(inbrowser=True)
+
+
+
+if __name__ == "__main__":
+    main()
