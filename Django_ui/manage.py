4bd31e8a71257c3d52fd3ac42750befe9ee023b0
ThommyBro <thomas.brockt@gmail.com>
2026-07-29 11:43:43 +0200

    initial django runs with extra students list via url/students

--- Django_ui/manage.py
+++ Django_ui/manage.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+# Add project root to Python path
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+)
 
 def main():
     """Run administrative tasks."""
--- Django_ui/tracker/views.py
+++ Django_ui/tracker/views.py
@@ -4,7 +4,67 @@ from django.shortcuts import render
 # Here starts my stuff
 
 from django.http import HttpResponse
+import sys
+import os
+
+from grade_store.sqlite_store import SqliteGradeStore
+from grade_db.student_repository import StudentRepository
+from grade_db.course_repository import CourseRepository
+from grade_db.grade_repository import GradeRepository
+from grade_db.statistics_repository import StatisticsRepository
+
+from grade_management.gradebook import GradeBook
+
+import sqlite3
+
+def get_store():
+
+
+    print("CURRENT PATH:")
+    print(os.getcwd())
+
+    db_path = "grade.db"
+
+    print("DATABASE:")
+    print(os.path.abspath(db_path))
+
+    con = sqlite3.connect(db_path)
+
+    student_repo = StudentRepository(con)
+    course_repo = CourseRepository(con)
+    grade_repo = GradeRepository(con)
+    stats_repo = StatisticsRepository(con)
+    gradebook = GradeBook()
+
+    return SqliteGradeStore(
+        student_repo,
+        course_repo,
+        grade_repo,
+        stats_repo,
+        gradebook
+    )
+
+
+def student_list(request):
+
+    store = get_store()
+    students = store.get_all_students()
+
+    print("NUMBER OF STUDENTS:", len(students))
+    print("STUDENTS:", students)
+
+    return render(
+        request,
+        "tracker/student_list.html",
+        {
+            "students": students
+        }
+    )
+
+    # print("STUDENT LIST VIEW START")
+    # return HttpResponse("Student view works")
+
 
 
 def home(request):
-    return HttpResponse("Hello Grade Tracker!")
+    return HttpResponse("Import funktioniert!")
